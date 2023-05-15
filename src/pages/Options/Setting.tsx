72ae6afdc14170811789518d637864d4f4d29222
takumibv <everybody.say.hey.yo@gmail.com>
2023-05-15 12:37:01 +0900

    書き出し実装

--- src/pages/Options/Setting.tsx
+++ src/pages/Options/Setting.tsx
@@ -6,16 +6,49 @@ import OptionHeader from "../../components/OptionHeader/OptionHeader";
 import * as sender from "../message/sender/options";
 import ColorPicker from "../../components/ColorPicker/ColorPicker";
 import { msg } from "../../utils";
+import { Note } from "../../types/Note";
+import { PageInfo } from "../../types/PageInfo";
+import { useNoteDownload } from "../../hooks/useNoteDownload";
+import Button from "../../components/Button/Button";
 
 interface Props extends RouteComponentProps<{}> {}
 
 const Setting: React.FC<Props> = () => {
   const [defaultColor, setDefaultColor] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [pageInfos, setPageInfos] = useState<PageInfo[]>([]);
+  const { handleDownload } = useNoteDownload();
+
   const onClickDefaultColor = (color: string) => {
     sender.sendUpdateDefaultColor(color).then(({ setting }) => {
       setDefaultColor(setting?.default_color ?? "");
     });
+  };
+
+  const handleDownloadCSV = () => {
+    const copyHead = "id, title, description, url\n";
+    const copyText = notes
+      .map((note) => {
+        return `${note.id}, ${note.title ? `"${note.title}"` : ""}, ${
+          note.description ? `"${note.description}"` : ""
+        }, ${pageInfos.find((pageInfo) => pageInfo.id === note.page_info_id)?.page_url}`;
+      })
+      .join("\n");
+    handleDownload(copyHead + copyText);
+  };
+
+  const handleDownloadText = () => {
+    const copyText = notes
+      .map((note) => {
+        return `id: ${note.id}
+title: ${note.title ?? ""}
+page: ${pageInfos.find((pageInfo) => pageInfo.id === note.page_info_id)?.page_url}
+content:
+${note.description ?? ""}`;
+      })
+      .join("\n------------------------------------------------------------\n");
+    handleDownload(copyText, "text/plain", "txt");
   };
 
   useEffect(() => {
@@ -28,6 +61,10 @@ const Setting: React.FC<Props> = () => {
       .finally(() => {
         setIsLoading(false);
       });
+    sender.sendFetchAllNotes().then(({ notes, pageInfos }) => {
+      notes && setNotes(notes);
+      pageInfos && setPageInfos(pageInfos);
+    });
   }, []);
 
   return (
@@ -53,16 +90,16 @@ const Setting: React.FC<Props> = () => {
                   <SSettingItem>
                     <SSettingItemTitle>書き出し</SSettingItemTitle>
                     <SSettingItemContent>
-                      <button>CSV</button>
-                      <button>text</button>
+                      <Button onClick={handleDownloadCSV}>CSVダウンロード</Button>
+                      <Button onClick={handleDownloadText} style={{ marginLeft: "0.5rem" }}>
+                        txtダウンロード
+                      </Button>
                     </SSettingItemContent>
                   </SSettingItem>
-                  <SSettingItem>
+                  {/* <SSettingItem>
                     <SSettingItemTitle>{msg("how_to_use_page_link_msg")}</SSettingItemTitle>
-                    <SSettingItemContent>
-                      使い方
-                    </SSettingItemContent>
-                  </SSettingItem>
+                    <SSettingItemContent>使い方</SSettingItemContent>
+                  </SSettingItem> */}
                   <SSettingItem>
                     <SSettingItemTitle>製作者</SSettingItemTitle>
                     <SSettingItemContent>
