affb0eb6fcb64d1fa696d0aca44ee6c6f9f50af9
takumibv <everybody.say.hey.yo@gmail.com>
2024-08-11 16:05:56 +0900

    [wip]vite設定

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,15 @@ import url from "node:url";
 import fs from "fs";
 import { createRequire } from "node:module";
 import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react-swc";
 import { crx, defineManifest } from "@crxjs/vite-plugin";
-
-const srcDir = path.join(__dirname, ".", "src");
 
 const manifest = defineManifest({
   manifest_version: 3,
   name: "__MSG_appName__",
   short_name: "__MSG_appShortName__",
   description: "__MSG_appDescription__",
-  version: "0.4.0",
+  version: process.env.npm_package_version ?? "1.0.0",
   default_locale: "en",
   icons: {
     16: "images/icon_16.png",
@@ -25,11 +24,11 @@ const manifest = defineManifest({
     service_worker: "src/pages/background/index.ts",
     type: "module",
   },
-  options_page: "memos.html",
+  options_page: "src/memos.html",
   action: {
-    default_popup: "popup.html",
+    default_popup: "src/popup.html",
   },
-  permissions: ["tabs", "contextMenus", "storage", "scripting", "webNavigation"],
+  permissions: ["tabs", "contextMenus", "storage", "scripting"],
   host_permissions: ["*://*/*"],
 });
 
@@ -39,11 +38,24 @@ export default defineConfig({
     reactVirtualized(),
     crx({
       manifest,
-      contentScripts: {
-        preambleCode: false,
-      },
+      // contentScripts: {
+      //   preambleCode: false,
+      // },
     }),
   ],
+  resolve: {
+    alias: {
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    },
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  }
 });
 
 // defineConfig({
